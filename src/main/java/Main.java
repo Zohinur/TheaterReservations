099f54786283c014c9d2952e756711f473497661
zohinurmiah <zohinur.m17@gmail.com>
2026-04-23 15:43:28 -0400

    Created main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+
+import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter your user name: ");
+        String userName = myScanner.nextLine();
+
+        System.out.println("Enter your date in this format: yyyy-MM-dd");
+        String dateInput = myScanner.nextLine();
+
+        LocalDate newDate = LocalDate.parse(dateInput);
+
+        System.out.println("How Many tickets would you like: ");
+        int tickets = myScanner.nextInt();
+
+        String s  = tickets +" ticket" + " ";
+        if (tickets > 1) {
+            s = tickets + "ticket" + "s";
+        }
+
+        System.out.println(userName + " " + newDate + " " + s);
+
+    }
 }
